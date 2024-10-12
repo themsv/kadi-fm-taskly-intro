ee926e9083229e51c755052ffea3eb77a37bdd40
Saivenkat Mahendrakar <themsv@Saivenkats-MacBook-Pro.local>
2024-10-12 22:29:17 +0530

    Create ShoppingListItem and reuse

--- App.tsx
+++ App.tsx
@@ -1,34 +1,15 @@
 import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { theme } from "./theme";
+import { ShoppingListItem } from "./components/ShoppingListItem";
 
 export default function App() {
-  const handleDelete = () => {
-    Alert.alert(
-      "Are you sure you want to delete this item?",
-      "Its gone forever!",
-      [
-        { text: "Cancel", style: "cancel" },
-        {
-          text: "Delete",
-          style: "destructive",
-          onPress: () => console.log("Deleted"),
-        },
-      ],
-    );
-  };
+ 
 
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <Text style={styles.itemText}>Coffee</Text>
-        <TouchableOpacity
-          style={styles.deleteButton}
-          onPress={handleDelete}
-          activeOpacity={0.7}
-        >
-          <Text style={styles.deleteButtonText}>Delete</Text>
-        </TouchableOpacity>
-      </View>
+      <ShoppingListItem name="Coffee" />
+      <ShoppingListItem name="Bread" />
+      <ShoppingListItem name="Milk" />
     </View>
   );
 }
@@ -38,29 +19,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: theme.colorWhite,
     justifyContent: "center",
-  },
-  itemContainer: {
-    paddingVertical: 16,
-    paddingHorizontal: 8,
-    borderBottomColor: theme.colorCerulean,
-    borderBottomWidth: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  itemText: {
-    fontSize: 18,
-    fontWeight: "200",
-  },
-  deleteButton: {
-    backgroundColor: theme.colorBlack,
-    padding: 8,
-    borderRadius: 4,
-  },
-  deleteButtonText: {
-    color: theme.colorWhite,
-    fontWeight: "bold",
-    textTransform: "uppercase",
-    letterSpacing: 1,
-  },
+  }, 
 });
