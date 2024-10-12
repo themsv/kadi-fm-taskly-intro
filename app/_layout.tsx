7bcee07458b2ccdab99234d0af7985278e88b6fa
Saivenkat Mahendrakar <themsv@Saivenkats-MacBook-Pro.local>
2024-10-12 23:20:19 +0530

    Modal Navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,18 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Shopping list" }} />
+      <Stack.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          presentation: "modal",
+          animation: "slide_from_bottom",
+        }}
+      />
+      <Stack.Screen
+        name="idea"
+        options={{ title: "My Idea", presentation: "modal" }}
+      />
     </Stack>
   );
 }
