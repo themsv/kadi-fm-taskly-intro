8bb996dbc1d3668c4e32e31a9a03af8b076e0ca0
Saivenkat Mahendrakar <themsv@Saivenkats-MacBook-Pro.local>
2024-10-14 20:48:49 +0530

    Add local notification for 5sec after click

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,11 +1,29 @@
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from "react-native";
 import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
 import { theme } from "../../theme";
+import * as Notifications from "expo-notifications";
+import { isDevice } from "expo-device";
 
 export default function CounterScreen() {
   const handleRequestPermission = async () => {
     const result = await registerForPushNotificationsAsync();
-    console.log("the result", result);
+    if (result === "granted") {
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "Plantly! I am notification, drink water",
+        },
+        trigger: {
+          seconds: 5,
+        },
+      });
+    } else {
+      if (isDevice) {
+        Alert.alert(
+          "Needs Permissions",
+          "Enable permission for notification for ExpoGo app in settings",
+        );
+      }
+    }
   };
   return (
     <View style={styles.container}>
@@ -14,7 +32,7 @@ export default function CounterScreen() {
         activeOpacity={0.2}
         style={styles.button}
       >
-        <Text style={styles.buttonText}>Counter</Text>
+        <Text style={styles.buttonText}>Schedule Notification</Text>
       </TouchableOpacity>
     </View>
   );
